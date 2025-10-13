eba9d3ebd67be3d5e1158802a70e670f4da7a51f
Jogonnath Das Talukder <cse_0182320012101060@lus.ac.bd>
2025-10-13 21:48:14 +0600

    Add files via upload

---
+++ 1st Assignment/Question4.dart
@@ -0,0 +1,8 @@
+//Write a program in Dart that finds simple interest
+void main() {
+    double p = 1000; // principal
+    double t = 2;    // time in years
+    double r = 5;    // rate
+    double simInt = (p * t * r) / 100;
+    print("Simple Interest = $simInt");
+}
---
+++ 1st Assignment/Question7.dart
@@ -0,0 +1,11 @@
+//Write a program to find quotient and remainder of two integers.
+void main() {
+    int a = 20;
+    int b = 3;
+
+    int quotient = a ~/ b;
+    int remainder = a % b;
+
+    print("Quotient: $quotient");
+    print("Remainder: $remainder");
+}
