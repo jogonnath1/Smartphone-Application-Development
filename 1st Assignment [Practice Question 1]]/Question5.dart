7a1a07ecaa8cc71b444837890c7c5898525fe455
Jogonnath Das Talukder <cse_0182320012101060@lus.ac.bd>
2025-11-02 21:41:49 +0600

    Update Question5.dart

--- 1st Assignment [Practice Question 1]]/Question5.dart
+++ 1st Assignment [Practice Question 1]]/Question5.dart
@@ -1,7 +1,8 @@
 //Write a program to print a square of a number using user input
 import 'dart:io';
+
 void main() {
-    print("Enter a number: ");
-    int? num = int.parse(stdin.readLineSync());
-    print("Square of $num is ${num * num}");
+  print("Enter a number:");
+  int num = int.parse(stdin.readLineSync()!);
+  print("Square of $num is ${num * num}");
 }
